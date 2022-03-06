2fe0ed6ccd185a4cc1214e9a0fad33fc4a9303e1
Yasmine Faiqotul H <arrizaalfirdausi@gmail.com>
2022-03-07 03:46:40 +0700

    add external script

---
+++ ext.js
@@ -0,0 +1,28 @@
+function catover() {
+    document.getElementById('catcard').src = "./assets/images/for aboutus/cat-2.png";
+}
+function catout() {
+    document.getElementById('catcard').src = "./assets/images/for aboutus/cat.png";
+}
+
+function dogover() {
+    document.getElementById('dogcard').src = "./assets/images/for aboutus/dog-2.png";
+}
+function dogout() {
+    document.getElementById('dogcard').src = "./assets/images/for aboutus/dog.png";
+}
+
+function submitform() {
+    alert('Your message have been saved, thank you');
+}
+
+function imagedown() {
+    document.getElementById('aboutusimage').src = "./assets/images/for aboutus/cat-n-dog-2.png";
+}
+function imageup() {
+    document.getElementById('aboutusimage').src = "./assets/images/for aboutus/cats-n-dog.png";
+}
+
+function welcome() {
+    alert('Welcome to Companion Corner! Click on the images to start')
+} 
